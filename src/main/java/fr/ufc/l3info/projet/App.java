09b382082b7162e43953567b00993dc66a08e86b
Sagona Loïc <loic.sagona@gmail.com>
2019-01-04 18:34:12 +0100

    init project

---
+++ src/main/java/fr/ufc/l3info/projet/App.java
@@ -0,0 +1,13 @@
+package fr.ufc.l3info.projet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
