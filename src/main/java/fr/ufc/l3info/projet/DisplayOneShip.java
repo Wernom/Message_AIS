a95be318eab11fbda74e0e2138ce7a1254e1bec0
MolagBaal <eloi.leroux1802@gmail.com>
2019-02-10 19:23:37 +0100

    correct bad selection in selection message

--- src/main/java/fr/ufc/l3info/projet/DisplayOneShip.java
+++ src/main/java/fr/ufc/l3info/projet/DisplayOneShip.java
@@ -41,10 +41,13 @@ class DisplayOneShip extends JPanel{
         listDeroulante.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
         listDeroulante.setLayoutOrientation(JList.VERTICAL);
         TreeMap<String, Message> mapMessage;
+        String selection;
         if(modif) {
             mapMessage = ship.getModifiedMessage();
+            selection=ship.getLastKnownModifiedMessage().getDecode().getUTCString();
         }else{
             mapMessage =ship.getMessages();
+            selection=ship.getLastKnownMessage().getDecode().getUTCString();
         }
         if(mapMessage.size()==0){
             defaultList.addElement("<empty>");
@@ -53,6 +56,7 @@ class DisplayOneShip extends JPanel{
             for (Message msg:mapMessage.values()) {
                 defaultList.addElement(msg.getDecode().getUTCString());
             }
+            listDeroulante.setSelectedValue(selection,false);
         }
         JScrollPane scrollPane=new JScrollPane(listDeroulante);
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
